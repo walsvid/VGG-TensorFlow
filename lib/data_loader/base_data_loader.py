1bdb935b0b1c2fed489968bd9cf6decabbaf6692
walsvid <walsvid@gmail.com>
2018-03-07 14:55:17 +0800

    walsvid - implement base data loader

---
+++ lib/data_loader/base_data_loader.py
@@ -0,0 +1,13 @@
+class DataLoader(object):
+    def __init__(self, cfg_):
+        """
+        :type cfg_: dict
+        :param cfg_: input config
+        """
+        self.config = cfg_
+
+    def load_data(self):
+        pass
+
+    def generate_batch(self):
+        pass
