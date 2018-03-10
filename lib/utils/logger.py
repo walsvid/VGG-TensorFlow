1d8c5f48226c6767f06e88df8a588d58a58e89f0
walsvid <walsvid@gmail.com>
2018-03-10 17:51:23 +0800

    walsvid - add logger

---
+++ lib/utils/logger.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import os
+
+
+class Logger(object):
+    def __init__(self, sess, config):
+        self.sess = sess
+        self.config = config
+        self.summary_placeholders = {}
+        self.summary_ops = {}
+        self.train_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "train"),
+                                                          self.sess.graph)
+        self.test_summary_writer = tf.summary.FileWriter(os.path.join(self.config.summary_dir, "test"))
