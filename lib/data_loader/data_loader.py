366747d7bd18c3021613897efe50294de0fd3dcb
walsvid <walsvid@gmail.com>
2018-03-11 16:45:39 +0800

    walsvid - remove input name scope inside function

--- lib/data_loader/data_loader.py
+++ lib/data_loader/data_loader.py
@@ -50,48 +50,46 @@ class CIFAR10BinDataLoader(DataLoader):
         self.filename_queue = self.load_data()
 
     def load_data(self):
-        with tf.name_scope('input'):
-            if self.is_train:
-                file_names = [os.path.join(self.data_dir, 'data_batch_%d.bin' % ii) for ii in np.arange(1, 6)]
-            else:
-                file_names = [os.path.join(self.data_dir, 'test_batch.bin')]
+        if self.is_train:
+            file_names = [os.path.join(self.data_dir, 'data_batch_%d.bin' % ii) for ii in np.arange(1, 6)]
+        else:
+            file_names = [os.path.join(self.data_dir, 'test_batch.bin')]
 
-            return tf.train.string_input_producer(file_names)
+        return tf.train.string_input_producer(file_names)
 
     def generate_batch(self):
-        with tf.name_scope('input'):
-            reader = tf.FixedLengthRecordReader(self.label_bytes + self.image_bytes)
-            key, value = reader.read(self.filename_queue)
+        reader = tf.FixedLengthRecordReader(self.label_bytes + self.image_bytes)
+        key, value = reader.read(self.filename_queue)
 
-            record_bytes = tf.decode_raw(value, tf.uint8)
+        record_bytes = tf.decode_raw(value, tf.uint8)
 
-            label = tf.slice(record_bytes, [0], [self.label_bytes])
-            label = tf.cast(label, tf.int32)
+        label = tf.slice(record_bytes, [0], [self.label_bytes])
+        label = tf.cast(label, tf.int32)
 
-            image_raw = tf.slice(record_bytes, [self.label_bytes], [self.image_bytes])
-            image_raw = tf.reshape(image_raw, [self.image_depth, self.image_height, self.image_width])
-            image = tf.transpose(image_raw, (1, 2, 0))  # convert from D/H/W to H/W/D
-            image = tf.cast(image, tf.float32)
+        image_raw = tf.slice(record_bytes, [self.label_bytes], [self.image_bytes])
+        image_raw = tf.reshape(image_raw, [self.image_depth, self.image_height, self.image_width])
+        image = tf.transpose(image_raw, (1, 2, 0))  # convert from D/H/W to H/W/D
+        image = tf.cast(image, tf.float32)
 
-            image = tf.image.per_image_standardization(image)
+        image = tf.image.per_image_standardization(image)
 
-            if self.is_shuffle:
-                image_batch, label_batch = tf.train.shuffle_batch([image, label],
-                                                                  batch_size=self.batch_size,
-                                                                  num_threads=64,
-                                                                  capacity=20000,
-                                                                  min_after_dequeue=3000)
-            else:
-                image_batch, label_batch = tf.train.batch([image, label],
-                                                          batch_size=self.batch_size,
-                                                          num_threads=64,
-                                                          capacity=2000)
+        if self.is_shuffle:
+            image_batch, label_batch = tf.train.shuffle_batch([image, label],
+                                                              batch_size=self.batch_size,
+                                                              num_threads=64,
+                                                              capacity=20000,
+                                                              min_after_dequeue=3000)
+        else:
+            image_batch, label_batch = tf.train.batch([image, label],
+                                                      batch_size=self.batch_size,
+                                                      num_threads=64,
+                                                      capacity=2000)
 
-            label_batch = tf.one_hot(label_batch, depth=self.n_classes)
-            label_batch = tf.cast(label_batch, dtype=tf.int32)
-            label_batch = tf.reshape(label_batch, [self.batch_size, self.n_classes])
+        label_batch = tf.one_hot(label_batch, depth=self.n_classes)
+        label_batch = tf.cast(label_batch, dtype=tf.int32)
+        label_batch = tf.reshape(label_batch, [self.batch_size, self.n_classes])
 
-            return image_batch, label_batch
+        return image_batch, label_batch
 
 
 def load_with_skip(data_path, session, skip_layer):
